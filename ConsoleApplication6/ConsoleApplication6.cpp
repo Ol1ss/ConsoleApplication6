2340892026cbd08b3a4ffca27882e7fc715338ae
olisss <olisss337@gmail.com>
2023-04-05 00:30:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+class Food {
+public:
+    virtual void prepare() = 0;
+};
+
+class Breakfast : public Food {
+public:
+    void prepare() override {
+        std::cout << "Preparing breakfast..." << std::endl;
+    }
+};
+
+class Lunch : public Food {
+public:
+    void prepare() override {
+        std::cout << "Preparing lunch..." << std::endl;
+    }
+};
+
+class Dinner : public Food {
+public:
+    void prepare() override {
+        std::cout << "Preparing dinner..." << std::endl;
+    }
+};
+
+class Person {
+public:
+    void eat(Food& food) {
+        food.prepare();
+        std::cout << "Eating " << typeid(food).name() << "..." << std::endl;
+    }
+};
